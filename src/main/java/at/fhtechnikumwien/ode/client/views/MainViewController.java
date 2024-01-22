2fe4de50b661a9ecc2fa29175c609ae5c047d6ab
Robert Castillo <robert.castillo@gmx.at>
2024-01-22 22:15:10 +0100

    Revert "new coomit"
    This reverts commit 8b5b331e9285efb59cf839f491b54905da9db86a.

--- src/main/java/at/fhtechnikumwien/ode/client/views/MainViewController.java
+++ src/main/java/at/fhtechnikumwien/ode/client/views/MainViewController.java
@@ -1,11 +1,14 @@
 package at.fhtechnikumwien.ode.client.views;
 
+import at.fhtechnikumwien.ode.common.Result;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
+import java.util.Objects;
 
-public class MainViewController {
+public class MainViewController implements MyView {
 
     public MainViewController(){
         /*FXMLLoader load = new FXMLLoader(getClass().getClassLoader().getResource("main-view.fxml"));
@@ -17,5 +20,15 @@ public class MainViewController {
         }catch(IOException e) {
             e.printStackTrace();
         }*/
+    }
+
+    @Override
+    public Result<Parent, String> getAsNode() {
+        try {
+            Parent node = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-view.fxml")));
+            return node != null ? Result.ok(node) : Result.err("Created node was null");
+        } catch (IOException e) {
+            return Result.err(e.getMessage());
+        }
     }
 }
